488650f7c1383a374cb019b898f337343c51dd15
ghdss-science <gustavotinho@gmail.com>
2024-05-30 19:06:50 -0300

    Desafio Realizado

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -1,16 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TrilhaApiDesafio.Models
 {
     public class Tarefa
     {
+       [Key]
         public int Id { get; set; }
+        
+        [Column(TypeName = "text")]
         public string Titulo { get; set; }
+        
+        [Column(TypeName = "text")]
         public string Descricao { get; set; }
+        
+        [Column(TypeName = "timestamp")]
         public DateTime Data { get; set; }
+        
         public EnumStatusTarefa Status { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,27 +1,28 @@
-using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using TrilhaApiDesafio.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adicionar o serviço do contexto do banco de dados
 builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
-builder.Services.AddControllers().AddJsonOptions(options =>
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Outros serviços
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configurar o middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => {
+
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"); 
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
